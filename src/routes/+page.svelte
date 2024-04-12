fd919dd3cb100832830b406beb423d76ca67109d
arnWolff <arnwolff@gmail.com>
2024-04-12 13:59:50 +0200

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
+<svelte:head>
+    <title>SAV DDTH-fr</title> 
+</svelte:head>
 <script>
   import { base } from "$app/paths";
 </script>
 
-<h1>sveltekit-gh-pages</h1>
+<h1>DDTH-FR-SAV</h1>
 <p>Deployed to GitHub Pages.</p>
 <a href="{base}/about">About</a>
