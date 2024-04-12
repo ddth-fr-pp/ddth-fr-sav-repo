21fd39e5448eca84dcf132eda6862a13c245af19
arnWolff <arnwolff@gmail.com>
2024-04-12 17:59:55 +0200

    up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
-<svelte:head>
-    <title>SAV DDTH-fr</title> 
-</svelte:head>
+<script>
+  import { base } from "$app/paths";
+</script>
 
-<main class="main">
-  <h1>SAV de DDTH-FR</h1>
-  <p>This is the body content.</p>
-  <!-- Add other body content here -->
-</main>
-
+<h1>DDTH-FR-SAV</h1>
+<p>Deployed to GitHub Pages.</p>
+<a href="{base}/about">About</a>
